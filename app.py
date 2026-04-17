b20806e52901be5bed1c4b7f8b590cf2685e934f
Aya-Abd-Elmoneem <147055404+Aya-Abd-Elmoneem@users.noreply.github.com>
2026-04-17 19:49:07 +0200

    Refactor import statement for load_qa_chain

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import FAISS
 
-from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import load_qa_chain
 from langchain.prompts import PromptTemplate
 
 # =========================
