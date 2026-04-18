eef83aed1bb58ca514264017737f49ef8f7c3282
Aya-Abd-Elmoneem <147055404+Aya-Abd-Elmoneem@users.noreply.github.com>
2026-04-18 14:34:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import re
 # CONFIG
 # =========================
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-flash-latest")
 
 st.set_page_config(page_title="SANAD AI", layout="wide")
 
